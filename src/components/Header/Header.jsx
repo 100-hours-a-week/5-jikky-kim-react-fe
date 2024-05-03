87aa17064833a71362d00bab373f56b67fc3f83a
jjikky <kok5417@skuniv.ac.kr>
2024-05-03 09:52:36 +0900

    💄 헤더 컴포넌트 구현

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+export default function Header() {
+    return (
+        <header>
+            <div id="header-flex">
+                <div id="header-text">아무 말 대잔치</div>
+            </div>
+        </header>
+    );
+}
