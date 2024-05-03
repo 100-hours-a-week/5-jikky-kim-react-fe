21953032ff37011933d9c4fa572db395a43f5f09
jjikky <kok5417@skuniv.ac.kr>
2024-05-03 16:15:26 +0900

    ♻️ 헤더 홈 가기 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
 import './Header.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 export default function Header() {
+    const NOT_LOGINED_PATH = ['/login', '/register'];
+    const location = useLocation();
+    const history = useNavigate();
+    const navigateToHome = () => {
+        history('/');
+    };
     return (
         <header>
-            <div id="header-flex">
-                <div id="header-text">아무 말 대잔치</div>
+            <div id='header-flex'>
+                <div
+                    id='header-text'
+                    onClick={!NOT_LOGINED_PATH.includes(location.pathname) ? navigateToHome : () => {}}
+                >
+                    아무 말 대잔치
+                </div>
             </div>
         </header>
     );
