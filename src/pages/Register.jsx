0b9bc35b03c97049010745e0788524e2df5377ad
jjikky <kok5417@skuniv.ac.kr>
2024-05-10 13:27:27 +0900

    🦺 회원가입 유효성검증 로직 분리

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,10 @@
 import style from './Register.module.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { validateEmail, validatePassword, validateNickname } from '../utils/validate';
+import { validateEmail, validatePassword, validateNickname, validateInput } from '../utils/validate';
 import { HELPER_TEXT } from '../constants/helperText';
 import { activateButton, deactivateButton, updateState, handleInputChange } from '../utils/utils';
+import api from '../utils/api';
 
 import Button from '../components/Button/Button';
 import Input from '../components/Input/Input';
@@ -28,86 +29,67 @@ export default function Register() {
         nicknameHelper: '',
     });
 
+    const registerForm = useRef();
+    const imageInput = useRef();
+    const preview = useRef();
+
+    const isValid =
+        validateEmail(user.email) &&
+        validatePassword(user.password) &&
+        validateNickname(user.nickname) === true &&
+        user.password === user.passwordCheck &&
+        user.profile;
+
     useEffect(() => {
-        // email 검증
-        if (user.email === '') updateState('emailHelper', HELPER_TEXT.EMAIL_EMPTY, setHelperText);
-        else {
-            // TODO : 중복 검사
-            validateEmail(user.email)
-                ? updateState('emailHelper', '', setHelperText)
-                : updateState('emailHelper', HELPER_TEXT.EMAIL_VALIDATION_FALSE, setHelperText);
-        }
+        // input 검증
+        validateInput.email(user.email, updateState, setHelperText);
+        validateInput.password(user.password, updateState, setHelperText);
+        validateInput.passwordCheck(user.password, user.passwordCheck, updateState, setHelperText);
+        validateInput.nickname(user.nickname, updateState, setHelperText);
 
-        // password 검증
-        if (user.password === '') updateState('passwordHelper', HELPER_TEXT.PASSWORD_EMPTY, setHelperText);
-        else {
-            validatePassword(user.password)
-                ? updateState('passwordHelper', '', setHelperText)
-                : updateState('passwordHelper', HELPER_TEXT.PASSWORD_VALIDATION_FALSE, setHelperText);
-        }
-
-        // passwordCheck 검증
-        if (user.passwordCheck === '')
-            updateState('passwordCheckHelper', HELPER_TEXT.PASSWORD_CHECK_EMPTY, setHelperText);
-        else {
-            user.password === user.passwordCheck
-                ? updateState('passwordCheckHelper', '', setHelperText)
-                : updateState('passwordCheckHelper', '비밀번호가 다릅니다.', setHelperText);
-        }
-
-        // nickname 검증
-        if (user.nickname === '') updateState('nicknameHelper', HELPER_TEXT.NICKNAME_EMPTY, setHelperText);
-        else {
-            // TODO : 중복 검사
-            let validateResult = validateNickname(user.nickname);
-            validateResult === true
-                ? updateState('nicknameHelper', '', setHelperText)
-                : updateState('nicknameHelper', HELPER_TEXT[validateResult], setHelperText);
-        }
-        if (
-            validateEmail(user.email) &&
-            validatePassword(user.password) &&
-            user.password === user.passwordCheck &&
-            user.nickname.length > 0 &&
-            validateNickname(user.nickname) === true &&
-            user.profile
-        ) {
+        if (isValid) {
             return activateButton('register-btn');
         }
         deactivateButton('register-btn');
-    }, [user]);
+    }, [user, isValid]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (
-            validateEmail(user.email) &&
-            validatePassword(user.password) &&
-            validateNickname(user.nickname) === true &&
-            user.profile
-        ) {
-            navigate('/login');
+        if (isValid) {
+            try {
+                const formData = new FormData(registerForm.current);
+                const response = await api.post('/users/register', formData);
+                console.log(response);
+                if (response?.message === 'user registered successfully') {
+                    return navigate('/login');
+                }
+            } catch (err) {
+                console.log(err);
+            }
         }
     };
 
     const fileInputHandler = async (event) => {
-        if (event.target.files && event.target.files[0]) {
+        const file = event.target.files[0];
+        if (event.target.files && file) {
             var reader = new FileReader();
-            reader.onload = function (event) {
-                document.getElementById('preview').src = event.target.result;
-            };
-            reader.readAsDataURL(event.target.files[0]);
-            document.getElementById('image-input').style.display = 'none';
-            document.getElementById('preview').style.display = 'block';
-            updateState('profile', event.target.files[0].name, setUser);
+            reader.onload = (event) => (preview.current.src = event.target.result);
+            reader.readAsDataURL(file);
+
+            imageInput.current.style.display = 'none';
+            preview.current.style.display = 'block';
+
+            updateState('profile', file.name, setUser);
             updateState('profileHelper', '', setHelperText);
+
             // preview에 input click 연결
-            document.getElementById('preview').addEventListener('click', function () {
+            preview.current.addEventListener('click', function () {
                 event.target.click();
             });
         } else {
-            document.getElementById('image-input').style.display = 'block';
-            document.getElementById('preview').style.display = 'none';
-            document.getElementById('preview').src = '';
+            imageInput.current.style.display = 'block';
+            preview.current.style.display = 'none';
+            preview.current.src = '';
             updateState('profile', '', setUser);
             updateState('profileHelper', HELPER_TEXT.PROFILE_EMPTY, setHelperText);
         }
@@ -117,11 +99,11 @@ export default function Register() {
         <main className={style.main}>
             <div className='page-title'>회원가입</div>
             <div className='flex'>
-                <form id='register-form'>
+                <form id='register-form' ref={registerForm} onSubmit={handleSubmit}>
                     <div className='label'>프로필 사진</div>
                     <div className='helper-text'>{helperText.profileHelper}</div>
                     <div className='flex'>
-                        <label id='image-input' className={style.image_input} htmlFor='profile'>
+                        <label id='image-input' ref={imageInput} className={style.image_input} htmlFor='profile'>
                             <div id='add-icon' className={style.add_icon}>
                                 +
                             </div>
@@ -129,7 +111,7 @@ export default function Register() {
                     </div>
 
                     <div className='flex'>
-                        <img id='preview' className={style.preview} alt='profile-preview' />
+                        <img id='preview' ref={preview} className={style.preview} alt='profile-preview' />
                     </div>
                     <Input
                         type='file'
@@ -187,7 +169,7 @@ export default function Register() {
                         />
                     </label>
                     <div className='helper-text'>{helperText.nicknameHelper}</div>
-                    <Button id='register-btn' text='회원가입' onSubmit={handleSubmit}></Button>
+                    <Button id='register-btn' text='회원가입'></Button>
                 </form>
             </div>
             <div className='go'>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,3 +1,5 @@
+import { HELPER_TEXT } from '../constants/helperText';
+
 export const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 export const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
 
@@ -19,5 +21,47 @@ export const validateNickname = (nickname) => {
     if (nickname.indexOf(' ') !== -1) return 'NICKNAME_VALIDATION_FALSE_SPACE';
     // 길이가 10글자 이하인지 확인
     if (nickname.length > 10) return 'NICKNAME_VALIDATION_FALSE_LENGTH';
+    if (nickname.length === 0) return 'NICKNAME_EMPTY';
     return true;
+};
+
+const duplicateNickname = (nickname) => {};
+
+export const validateInput = {
+    email: (email, setState, setHelperText) => {
+        if (email === '') setState('emailHelper', HELPER_TEXT.EMAIL_EMPTY, setHelperText);
+        else {
+            // TODO : 중복 검사
+            validateEmail(email)
+                ? setState('emailHelper', '', setHelperText)
+                : setState('emailHelper', HELPER_TEXT.EMAIL_VALIDATION_FALSE, setHelperText);
+        }
+    },
+    password: (password, setState, setHelperText) => {
+        if (password === '') setState('passwordHelper', HELPER_TEXT.PASSWORD_EMPTY, setHelperText);
+        else {
+            validatePassword(password)
+                ? setState('passwordHelper', '', setHelperText)
+                : setState('passwordHelper', HELPER_TEXT.PASSWORD_VALIDATION_FALSE, setHelperText);
+        }
+    },
+    passwordCheck: (password, passwordCheck, setState, setHelperText) => {
+        if (passwordCheck === '') setState('passwordCheckHelper', HELPER_TEXT.PASSWORD_CHECK_EMPTY, setHelperText);
+        else {
+            password === passwordCheck
+                ? setState('passwordCheckHelper', '', setHelperText)
+                : setState('passwordCheckHelper', '비밀번호가 다릅니다.', setHelperText);
+        }
+    },
+    nickname: (nickname, setState, setHelperText) => {
+        if (nickname === '') setState('nicknameHelper', HELPER_TEXT.NICKNAME_EMPTY, setHelperText);
+        else {
+            // TODO : 중복 검사
+            // let isDuplicate = isDuplicateNickname(nickname);
+            let isValidate = validateNickname(nickname);
+            isValidate === true
+                ? setState('nicknameHelper', '', setHelperText)
+                : setState('nicknameHelper', HELPER_TEXT[isValidate], setHelperText);
+        }
+    },
 };
