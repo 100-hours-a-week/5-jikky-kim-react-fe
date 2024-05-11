e27d19c9ddae9ab29abaaae87a33d3e43377f181
jjikky <kok5417@skuniv.ac.kr>
2024-05-11 15:19:46 +0900

    🐛 헤더 유저 이미지 오류 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,18 @@ import WithLogin from './WithLogin';
 
 export default function Header() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [_, setPath] = useState('');
     const NOT_LOGINED_PATH = ['/login', '/register'];
     const location = useLocation();
+
     useEffect(() => {
+        setPath(location.pathname);
         if (NOT_LOGINED_PATH.includes(location.pathname)) {
             return setIsLoggedIn(false);
         }
         setIsLoggedIn(true);
-    }, []);
+    }, [location.pathname]);
+
     return (
         <header className={style.header}>
             <div id='header-flex' className={style.header_flex}>
