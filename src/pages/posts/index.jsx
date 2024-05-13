39217408e2264c8b0e39f76b23f2a215ea9ee178
jjikky <kok5417@skuniv.ac.kr>
2024-05-13 17:18:11 +0900

    ♻️ 게시글 route 경로 변경

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Posts from './Posts';
-import PostDetail from './PostDetail.jsx';
 import PostCreate from './post-create';
-import PostUpdate from './PostUpdate.jsx';
+import Post from './post';
+import PostUpdate from './post-update';
 
 function index() {
     return (
         <Routes>
             <Route path='' element={<Posts />} />
             <Route path='create' element={<PostCreate />} />
-            <Route path=':id' element={<PostDetail />} />
-            <Route path=':id/update' element={<PostUpdate />} />
+            <Route path='/:id' element={<Post />} />
+            <Route path='/:id/update' element={<PostUpdate />} />
         </Routes>
     );
 }
