36510d9cd19f949c1ad2e1743a471f8332664f25
jjikky <kok5417@skuniv.ac.kr>
2024-05-13 17:19:33 +0900

    🎨 게시글 조회 폴더 구조 변경

--- src/pages/posts/PostUpdate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function PostUpdate() {
-    return <div>PostUpdate</div>;
-}
-
-export default PostUpdate;
---
+++ src/pages/posts/post/Post.jsx
@@ -0,0 +1,7 @@
+import style from './Post.module.css';
+
+function Post() {
+    return <div>Post</div>;
+}
+
+export default Post;
