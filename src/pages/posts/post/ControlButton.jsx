033ee81cfa24ef6004942f5f3f931577f2a75712
jjikky <kok5417@skuniv.ac.kr>
2024-05-14 15:39:07 +0900

    ✨ 수정 삭제 컨트롤 버튼 컴포넌트 구현

---
+++ src/pages/posts/post/ControlButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './ControlButton.module.css';
+
+function ControlButton({ updateButtonClickHandler, deleteButtonClickHandler }) {
+    return (
+        <div className={style.comment_item}>
+            <button className={style.comment_item_button} onClick={updateButtonClickHandler}>
+                수정
+            </button>
+            <button
+                className={`${style.comment_item_button} ${style.del_comment_modal_btn}`}
+                onClick={deleteButtonClickHandler}
+            >
+                삭제
+            </button>
+        </div>
+    );
+}
+
+export default ControlButton;
