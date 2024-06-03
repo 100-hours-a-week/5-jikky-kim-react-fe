a873fc46f30063d809fb9788358d35bc3b061940
jjikky <kok5417@skuniv.ac.kr>
2024-06-03 18:29:01 +0900

    ♻️ 게시글 목록 조회 api 형식 변경

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './PostCard.module.css';
 import { Link } from 'react-router-dom';
-import { formatCount, titleSlice } from '../../utils/utils';
+import { formatCount, titleSlice, formatDate } from '../../utils/utils';
 import Line from '../Line/Line';
 
 const Post = (props) => {
-    const { post_id, title, created_at, creator, count, comments } = props;
+    const { post_id, title, created_at, creator_nickname, creator_avatar, count_like, count_comment, count_view } =
+        props;
     return (
         <>
             <Link to={'/posts/' + post_id}>
@@ -13,16 +14,16 @@ const Post = (props) => {
                     <div className={style.post_title}>{titleSlice(title)}</div>
                     <div className={style.post_mid}>
                         <div className={style.post_mid_l}>
-                            <div className={style.count_like}>좋아요 {formatCount(count.like)}</div>
-                            <div className={style.count_comment}>댓글 {formatCount(comments.length)}</div>
-                            <div className={style.count_view}>조회수 {formatCount(count.view)}</div>
+                            <div className={style.count_like}>좋아요 {formatCount(count_like)}</div>
+                            <div className={style.count_comment}>댓글 {formatCount(count_comment)}</div>
+                            <div className={style.count_view}>조회수 {formatCount(count_view)}</div>
                         </div>
-                        <div className={style.created_at}>{created_at}</div>
+                        <div className={style.created_at}>{formatDate(created_at)}</div>
                     </div>
                     <Line />
                     <div className={style.post_creator}>
-                        <img className={style.avatar} alt='avatar' src={creator.avatar} />
-                        <div className={style.creator}>{creator.nickname}</div>
+                        <img className={style.avatar} alt='avatar' src={creator_avatar} />
+                        <div className={style.creator}>{creator_nickname}</div>
                     </div>
                 </div>
             </Link>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 // 버튼 활성화, 비활성화
 export const activateButton = (id) => {
     const button = document.getElementById(id);
@@ -29,6 +31,7 @@ export const handleSingleInputChange = (e, setState) => {
 
 // 조회수,댓글,좋아요 수 등 형식 변환
 export const formatCount = (count) => {
+    count *= 1;
     if (count >= 1000000) {
         return (count / 1000000).toFixed(1) + 'm';
     } else if (count >= 1000) {
@@ -43,4 +46,8 @@ export const titleSlice = (title) => {
     const TITLE_MAX_LENGTH = 26;
     if (title.length > TITLE_MAX_LENGTH) return title.substring(0, TITLE_MAX_LENGTH);
     return title;
+};
+
+export const formatDate = (isoDate) => {
+    return moment(isoDate).format('YYYY-MM-DD HH:mm:ss');
 };
