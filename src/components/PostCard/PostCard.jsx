5afadb365e0316f984cd51f4c1d0251149cbc283
jjikky <kok5417@skuniv.ac.kr>
2024-06-03 22:10:57 +0900

    ♻️ 게시글 목록 조회 style변경

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -6,13 +6,28 @@ import Line from '../Line/Line';
 import { IMAGE_SERVER_URL } from '../../constants/res';
 
 const Post = (props) => {
-    const { post_id, title, created_at, creator_nickname, creator_avatar, count_like, count_comment, count_view } =
-        props;
+    const {
+        post_id,
+        title,
+        post_image,
+        created_at,
+        creator_nickname,
+        creator_avatar,
+        count_like,
+        count_comment,
+        count_view,
+    } = props;
     return (
         <>
             <Link to={'/posts/' + post_id}>
                 <div className={style.post}>
+                    <div className={style.post_creator}>
+                        <img className={style.avatar} alt='avatar' src={IMAGE_SERVER_URL + creator_avatar} />
+                        <div className={style.creator}>{creator_nickname}</div>
+                    </div>
+                    <img className={style.thumbnail_img} alt='thumbnail' src={IMAGE_SERVER_URL + post_image}></img>
                     <div className={style.post_title}>{titleSlice(title)}</div>
+
                     <div className={style.post_mid}>
                         <div className={style.post_mid_l}>
                             <div className={style.count_like}>좋아요 {formatCount(count_like)}</div>
@@ -20,11 +35,6 @@ const Post = (props) => {
                             <div className={style.count_view}>조회수 {formatCount(count_view)}</div>
                         </div>
                         <div className={style.created_at}>{formatDate(created_at)}</div>
-                    </div>
-                    <Line />
-                    <div className={style.post_creator}>
-                        <img className={style.avatar} alt='avatar' src={IMAGE_SERVER_URL + creator_avatar} />
-                        <div className={style.creator}>{creator_nickname}</div>
                     </div>
                 </div>
             </Link>
