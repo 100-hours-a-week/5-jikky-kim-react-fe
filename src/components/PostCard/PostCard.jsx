826d4ebd41fb496c568f3aa5a6f3aec1cf182623
jjikky <kok5417@skuniv.ac.kr>
2024-06-18 17:12:51 +0900

    ♻️ 게시글 목록 조회 바뀐 데이터 형식 적용

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -6,35 +6,26 @@ import Line from '../Line/Line';
 import { IMAGE_SERVER_URL } from '../../constants/res';
 
 const Post = (props) => {
-    const {
-        post_id,
-        title,
-        post_image,
-        created_at,
-        creator_nickname,
-        creator_avatar,
-        count_like,
-        count_comment,
-        count_view,
-    } = props;
+    const { postId, title, postImage, createdAt, creatorNickname, creatorAvatar, countLike, countComment, countView } =
+        props;
     return (
         <>
-            <Link to={'/posts/' + post_id}>
+            <Link to={'/posts/' + postId}>
                 <div className={style.post}>
                     <div className={style.post_creator}>
-                        <img className={style.avatar} alt='avatar' src={IMAGE_SERVER_URL + creator_avatar} />
-                        <div className={style.creator}>{creator_nickname}</div>
+                        <img className={style.avatar} alt='avatar' src={IMAGE_SERVER_URL + creatorAvatar} />
+                        <div className={style.creator}>{creatorNickname}</div>
                     </div>
-                    <img className={style.thumbnail_img} alt='thumbnail' src={IMAGE_SERVER_URL + post_image}></img>
+                    <img className={style.thumbnail_img} alt='thumbnail' src={IMAGE_SERVER_URL + postImage}></img>
                     <div className={style.post_title}>{titleSlice(title)}</div>
 
                     <div className={style.post_mid}>
                         <div className={style.post_mid_l}>
-                            <div className={style.count_like}>좋아요 {formatCount(count_like)}</div>
-                            <div className={style.count_comment}>댓글 {formatCount(count_comment)}</div>
-                            <div className={style.count_view}>조회수 {formatCount(count_view)}</div>
+                            <div className={style.countLike}>좋아요 {formatCount(countLike)}</div>
+                            <div className={style.countComment}>댓글 {formatCount(countComment)}</div>
+                            <div className={style.countView}>조회수 {formatCount(countView)}</div>
                         </div>
-                        <div className={style.created_at}>{formatDate(created_at)}</div>
+                        <div className={style.createdAt}>{formatDate(createdAt)}</div>
                     </div>
                 </div>
             </Link>
